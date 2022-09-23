be057774dae9e780c5377cd56d487e80fd513d49
LazyUnigineDude <lazy.unigine@gmail.com>
2022-09-22 20:24:24 -0400

    AI

--- data/Scripts/AI/PathMaker.cs
+++ data/Scripts/AI/PathMaker.cs
@@ -6,7 +6,6 @@ using Unigine;
 [Component(PropertyGuid = "7085ad171d6ee78a87cbb1619cec9b4ca4bf1e3b")]
 public class PathMaker : Component
 {
-	public Node Obj2Move;
 	public int TimeBetweenPoints = 5;
 	public List<Node> PathPoints;
 
@@ -14,30 +13,30 @@ public class PathMaker : Component
 	float Weight = 0.0f;
 	int num = 0;
 
-	private void Init()
-	{
-		// write here code to be called on component initialization
+	public void InitPath() {
+		
 		Path = new();
-
 		for (int i = 0; i < PathPoints.Count; i++) { Path.AddPoint(PathPoints[i].WorldPosition); }
+		for (int i = 0; i < PathPoints.Count; i++) {
+			int num = i % Path.NumPoints, num2 = (i + 1) % Path.NumPoints;
 
-		Path.AddSegment(0, PathPoints[0].GetWorldDirection(MathLib.AXIS.Y), PathPoints[0].GetWorldDirection(MathLib.AXIS.Z),
-						1, PathPoints[1].GetWorldDirection(MathLib.AXIS.NY), PathPoints[1].GetWorldDirection(MathLib.AXIS.Z));
-		Path.AddSegment(1, PathPoints[1].GetWorldDirection(MathLib.AXIS.Y), PathPoints[1].GetWorldDirection(MathLib.AXIS.Z),
-						2, PathPoints[2].GetWorldDirection(MathLib.AXIS.NY), PathPoints[2].GetWorldDirection(MathLib.AXIS.Z));
-		Path.AddSegment(2, PathPoints[2].GetWorldDirection(MathLib.AXIS.Y), PathPoints[2].GetWorldDirection(MathLib.AXIS.Z),
-						3, PathPoints[3].GetWorldDirection(MathLib.AXIS.NY), PathPoints[3].GetWorldDirection(MathLib.AXIS.Z));
-		Path.AddSegment(3, PathPoints[3].GetWorldDirection(MathLib.AXIS.Y), PathPoints[3].GetWorldDirection(MathLib.AXIS.Z),
-						0, PathPoints[0].GetWorldDirection(MathLib.AXIS.NY), PathPoints[0].GetWorldDirection(MathLib.AXIS.Z));
-
+			Path.AddSegment(num, PathPoints[num].GetWorldDirection(MathLib.AXIS.Y), PathPoints[num].GetWorldDirection(MathLib.AXIS.Z),
+							num2, PathPoints[num2].GetWorldDirection(MathLib.AXIS.NY), PathPoints[num2].GetWorldDirection(MathLib.AXIS.Z));
+		}
 	}
-	
-	private void Update()
-	{
-		// write here code to be called before updating each render frame
+
+	public void MoveAlongPath() {
+		
 		Weight = MathLib.Clamp(Weight += (Game.IFps / TimeBetweenPoints), 0.0f, 1.0f);
 		if (Weight == 1.0f) { Weight = 0; num++; }
 		num %= Path.NumPoints;
+	}
+
+	public vec3 GetCurrentPathPosition() {
+		return Path.CalcSegmentPoint(num, Weight);
+    }
+
+	public void MoveObject(Node Obj2Move) {
 
 		vec3 Point = Path.CalcSegmentPoint(num, Weight),
 			 Direc = Path.CalcSegmentTangent(num, Weight),
@@ -45,14 +44,11 @@ public class PathMaker : Component
 
 		Obj2Move.WorldPosition = Point;
 		Obj2Move.SetWorldDirection(Direc, UpVec, MathLib.AXIS.Y);
-
-		RenderPath();
 	}
 
-	void RenderPath()
-    {
+	public void RenderPath()  {
+		
 		int segments = 50;
-
         for (int i = 0; i < Path.NumPoints; i++)
         {
 			Visualizer.RenderPoint3D(Path.GetPoint(i), 0.1f, vec4.BLACK);
--- data/Scripts/AI/ShooterAI.cs
+++ data/Scripts/AI/ShooterAI.cs
@@ -6,6 +6,7 @@ using Unigine;
 [Component(PropertyGuid = "1fa4e0e57fc69d003112ec5f59d9ce32b78857e9")]
 public class ShooterAI : Component
 {
+	public Node PathMakerNode;
 	public Node MainCharacter;
 	private float ViewDistance, Weight, DistanceRatio;
 
@@ -14,7 +15,9 @@ public class ShooterAI : Component
 
 	enum AISTATE { IDLE, ALERT, SEARCH, AGGRESSIVE}
 	AISTATE STATE;
-
+	quat HorReset = new quat(90, 0, 0);
+	PathMaker Path;
+	mat4 View;
 	private void Init()
 	{
 		// write here code to be called on component initialization
@@ -22,6 +25,10 @@ public class ShooterAI : Component
 		ViewDistance = 15;
 		Weight = 0;
 		STATE = AISTATE.IDLE;
+		Path = GetComponent<PathMaker>(PathMakerNode);
+		BF = new();
+		View = new();
+		Path.InitPath();
 	}
 	
 	private void Update()
@@ -29,12 +36,12 @@ public class ShooterAI : Component
 		// write here code to be called before updating each render frame
 		
 		mat4 Frustum = MathLib.Perspective(40, 1.5f, 0.05f, ViewDistance);
-			quat Rotation = node.GetWorldRotation() * new quat(90, 0, 0);
-			mat4 View = new(Rotation, node.GetChild(0).WorldPosition);
+			quat Rotation = node.GetWorldRotation() * HorReset;
+		vec3 Pos = node.GetChild(0).WorldPosition;
+		 View.Set(Rotation, Pos);
 		
-		Visualizer.RenderFrustum(Frustum, View, vec4.BLACK);
-
-		BF = new(Frustum, MathLib.Inverse(View));
+		Visualizer.RenderFrustum(Frustum, View, vec4.BLACK); 
+		BF.Set(Frustum, MathLib.Inverse(View));
 
 		if (BF.Inside(MainCharacter.WorldPosition))
 		{
@@ -43,7 +50,8 @@ public class ShooterAI : Component
 			DistanceRatio = distance / ViewDistance;
 		}
 		else isInsideFrustum = false;
-
+		
+		Path.RenderPath();
 		AiSTATE();
 	}
 
@@ -55,25 +63,57 @@ public class ShooterAI : Component
 				Log.Message("IDLE\n");
 				Weight = MathLib.Clamp(Weight -= Game.IFps, 0f, 1f);
 				if (isInsideFrustum) STATE = AISTATE.ALERT;
+                if (MathLib.Distance(node.WorldPosition, Path.GetCurrentPathPosition()) > 0.1f)
+                {
+					MoveTowards(Path.GetCurrentPathPosition(), node);
+					RotateTowards(Path.GetCurrentPathPosition(), node, 0.05f);
+                }
+                else
+                {
+					Path.MoveAlongPath();
+					Path.MoveObject(node);
+                }
 				break;
 			case AISTATE.ALERT:
 				Log.Message("ALRT\n");
 				Weight = MathLib.Clamp(Weight += Game.IFps / DistanceRatio, 0f, 1f);
 				if (!isInsideFrustum) STATE = AISTATE.IDLE;
 				if (Weight == 1f) STATE = AISTATE.AGGRESSIVE;
+
+					RotateTowards(MainCharacter.WorldPosition, node, 0.005f);
 				break;
 			case AISTATE.SEARCH:
 				Log.Message("SRCH\n");
 				Weight = MathLib.Clamp(Weight -= Game.IFps / 5, 0f, 1f);
 				if (Weight == 0f) STATE = AISTATE.IDLE;
 				if (isInsideFrustum) { STATE = AISTATE.AGGRESSIVE; Weight = 1; }
+					RotateTowards(MainCharacter.WorldPosition, node, 0.05f);
 				break;
             case AISTATE.AGGRESSIVE:
 				Log.Message("AGRO\n");
 				if (!isInsideFrustum) STATE = AISTATE.SEARCH;
-                break;
+					MoveTowards(MainCharacter.WorldPosition, node);
+					RotateTowards(MainCharacter.WorldPosition, node, 0.05f);
+				break;
             default:
                 break;
         }
     }
+
+	void RotateTowards(vec3 TowardsObject, Node Obj2Move, float Speed) {
+
+		vec3 Vec1 = Obj2Move.GetWorldDirection(MathLib.AXIS.Y),
+			 Vec2 = (TowardsObject - Obj2Move.WorldPosition).Normalized;
+		float Angle = MathLib.Angle(Vec1, Vec2, vec3.UP);
+		Obj2Move.Rotate(-Obj2Move.GetWorldRotation().x, -Obj2Move.GetWorldRotation().y, Angle * Speed);
+	}
+
+	void MoveTowards(vec3 TowardsObject, Node Obj2Move) {
+
+		vec3 Pos = MathLib.Lerp(Obj2Move.WorldPosition,
+								TowardsObject,
+								Game.IFps / MathLib.Distance(Obj2Move.WorldPosition,
+															TowardsObject));
+		Obj2Move.WorldPosition = Pos;
+	}
 }
